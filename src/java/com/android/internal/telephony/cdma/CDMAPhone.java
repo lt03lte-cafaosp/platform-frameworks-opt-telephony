7d33e6ba0def44ec4bd206e9b9dd3bcaf75df11e
Anshul Jain <anshulj@codeaurora.org>
2011-11-22 12:23:14 -0800

    Telephony: Handle RIL_UNSOL_EXIT_EMERGENCY_CALLBACK_MODE
    Handle RIL_UNSOL_EXIT_EMERGENCY_CALLBACK_MODE as a way to exit from
    Emergency Callback Mode
    
    Change-Id: Ia572ac5a4d5ab38166453dcdf26fb0a667bba74e

--- src/java/com/android/internal/telephony/cdma/CDMAPhone.java
+++ src/java/com/android/internal/telephony/cdma/CDMAPhone.java
@@ -182,6 +182,8 @@ public class CDMAPhone extends PhoneBase {
         mCM.setOnSuppServiceNotification(this, EVENT_SSN, null);
         mSST.registerForNetworkAttached(this, EVENT_REGISTERED_TO_NETWORK, null);
         mCM.setEmergencyCallbackMode(this, EVENT_EMERGENCY_CALLBACK_MODE_ENTER, null);
+        mCM.registerForExitEmergencyCallbackMode(this, EVENT_EXIT_EMERGENCY_CALLBACK_RESPONSE,
+                null);
 
         PowerManager pm
             = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
@@ -236,6 +238,7 @@ public class CDMAPhone extends PhoneBase {
             mCM.unregisterForOn(this); //EVENT_RADIO_ON
             mSST.unregisterForNetworkAttached(this); //EVENT_REGISTERED_TO_NETWORK
             mCM.unSetOnSuppServiceNotification(this);
+            mCM.unregisterForExitEmergencyCallbackMode(this);
             removeCallbacks(mExitEcmRunnable);
 
             mPendingMmis.clear();
