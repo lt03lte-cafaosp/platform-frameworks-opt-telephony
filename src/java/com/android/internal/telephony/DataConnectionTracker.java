12ebb4d799d30d6cd1774534c038a772900161e7
Alex Yakavenka <ayakav@codeaurora.org>
2012-06-20 16:20:31 -0700

    Telephony: Ignore data registration when bringing down call
    Bring down data call even if we have it while data registration indicates
    phone is in unregistered state.
    getAnyDataEnabled will return false if phone is not registered hence
    user disabling data while phone is unregistered will have no effect
    (and data call will remain there indefinitely).
    
    Change-Id: I72cf1fb8b89cad5bb8595233721d1ac315750dbf
    CRs-Fixed: 372219

--- src/java/com/android/internal/telephony/DataConnectionTracker.java
+++ src/java/com/android/internal/telephony/DataConnectionTracker.java
@@ -1107,7 +1107,6 @@ public abstract class DataConnectionTracker extends Handler {
 
     protected void onSetUserDataEnabled(boolean enabled) {
         synchronized (mDataEnabledLock) {
-            final boolean prevEnabled = getAnyDataEnabled();
             if (mUserDataEnabled != enabled) {
                 mUserDataEnabled = enabled;
                 Settings.Global.putInt(mPhone.getContext().getContentResolver(),
@@ -1120,13 +1119,12 @@ public abstract class DataConnectionTracker extends Handler {
                         notifyOffApnsOfAvailability(Phone.REASON_DATA_DISABLED);
                     }
                 }
-                if (prevEnabled != getAnyDataEnabled()) {
-                    if (!prevEnabled) {
-                        resetAllRetryCounts();
-                        onTrySetupData(Phone.REASON_DATA_ENABLED);
-                    } else {
-                        onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
-                    }
+
+                if (enabled) {
+                    resetAllRetryCounts();
+                    onTrySetupData(Phone.REASON_DATA_ENABLED);
+                } else {
+                    onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
                 }
             }
         }
@@ -1137,16 +1135,13 @@ public abstract class DataConnectionTracker extends Handler {
 
     protected void onSetPolicyDataEnabled(boolean enabled) {
         synchronized (mDataEnabledLock) {
-            final boolean prevEnabled = getAnyDataEnabled();
             if (sPolicyDataEnabled != enabled) {
                 sPolicyDataEnabled = enabled;
-                if (prevEnabled != getAnyDataEnabled()) {
-                    if (!prevEnabled) {
-                        resetAllRetryCounts();
-                        onTrySetupData(Phone.REASON_DATA_ENABLED);
-                    } else {
-                        onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
-                    }
+                if (enabled) {
+                    resetAllRetryCounts();
+                    onTrySetupData(Phone.REASON_DATA_ENABLED);
+                } else {
+                    onCleanUpAllConnections(Phone.REASON_DATA_DISABLED);
                 }
             }
         }
