a43ef6c1dd99fe220eafce24072da3dcfe1a9dd6
Jiju Kinattingal <jijuk@codeaurora.org>
2012-11-01 18:22:42 +0530

    Telephony(DSDS): Xdivert enhancements.
    Added registerForSimRecordsLoaded function which is
    required for XDivert implementation
    
    Change-Id: I4d1a20a1276654177595c48bc6837207aae2c6df

--- src/java/com/android/internal/telephony/gsm/GSMPhone.java
+++ src/java/com/android/internal/telephony/gsm/GSMPhone.java
@@ -427,6 +427,16 @@ public class GSMPhone extends PhoneBase {
         if (mSsnRegistrants.size() == 0) mCM.setSuppServiceNotifications(false, null);
     }
 
+    @Override
+    public void registerForSimRecordsLoaded(Handler h, int what, Object obj) {
+        mSimRecordsLoadedRegistrants.addUnique(h, what, obj);
+    }
+
+    @Override
+    public void unregisterForSimRecordsLoaded(Handler h) {
+        mSimRecordsLoadedRegistrants.remove(h);
+    }
+
     public void
     acceptCall() throws CallStateException {
         mCT.acceptCall();
@@ -1202,6 +1212,7 @@ public class GSMPhone extends PhoneBase {
                     setVmSimImsi(null);
                 }
 
+                mSimRecordsLoadedRegistrants.notifyRegistrants();
             break;
 
             case EVENT_GET_BASEBAND_VERSION_DONE:
