gp-b2g/frameworks_base

Telephony: Use DATA_REGISTRATION_STATE for data
Use reg state from DATA_REGISTRATION_STATE when in LTE/EHRPD
and also for CDMA. Without this fix, the radiotech in voice
reg. state would get processed for CDMA and data calls get
attempted prematurely.

Change-Id: I1509a6ffe33fc0fc87661aa5d955a61597daf10c
CRs-Fixed: 323323

telephony/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -189,20 +189,11 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
 
     @Override
     protected void pollStateDone() {
-        // determine data NetworkType from both LET and CDMA SS
-        if (mLteSS.getState() == ServiceState.STATE_IN_SERVICE) {
-            //in LTE service
-            newNetworkType = mLteSS.getRadioTechnology();
-            mNewDataConnectionState = mLteSS.getState();
-            newSS.setRadioTechnology(newNetworkType);
-            log("pollStateDone LTE/eHRPD STATE_IN_SERVICE newNetworkType = " + newNetworkType);
-        } else {
-            // LTE out of service, get CDMA Service State
-            newNetworkType = newSS.getRadioTechnology();
-            mNewDataConnectionState = radioTechnologyToDataServiceState(newNetworkType);
-            log("pollStateDone CDMA STATE_IN_SERVICE newNetworkType = " + newNetworkType +
-                " mNewDataConnectionState = " + mNewDataConnectionState);
-        }
+        newNetworkType = mLteSS.getRadioTechnology();
+        mNewDataConnectionState = mLteSS.getState();
+        newSS.setRadioTechnology(newNetworkType);
+        log("pollStateDone CdmaLTEServiceState STATE_IN_SERVICE newNetworkType = "
+                + newNetworkType);
 
         // TODO: Add proper support for LTE Only, we should be looking at
         //       the preferred network mode, to know when newSS state should
@@ -437,7 +428,7 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
 
     @Override
     public boolean isConcurrentVoiceAndDataAllowed() {
-        if (RadioTechnology.getRadioTechFromInt(mLteSS.getRadioTechnology()) != 
+        if (RadioTechnology.getRadioTechFromInt(mLteSS.getRadioTechnology()) !=
                     RadioTechnology.RADIO_TECH_1xRTT)
             return SystemProperties.getBoolean(TelephonyProperties.PROPERTY_SVDATA, false);
         else

lt03lte-cafaosp/platform-frameworks-opt-telephony

Conflicts:
	telephony/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java

src/java/com/android/internal/telephony/cdma/CdmaLteServiceStateTracker.java
@@ -259,22 +259,11 @@ public class CdmaLteServiceStateTracker extends CdmaServiceStateTracker {
 
     @Override
     protected void pollStateDone() {
-        // determine data RadioTechnology from both LET and CDMA SS
-        if (mLteSS.getState() == ServiceState.STATE_IN_SERVICE) {
-            //in LTE service
-            mNewRilRadioTechnology = mLteSS.getRilRadioTechnology();
-            mNewDataConnectionState = mLteSS.getState();
-            newSS.setRadioTechnology(mNewRilRadioTechnology);
-            log("pollStateDone LTE/eHRPD STATE_IN_SERVICE mNewRilRadioTechnology = " +
-                    mNewRilRadioTechnology);
-        } else {
-            // LTE out of service, get CDMA Service State
-            mNewRilRadioTechnology = newSS.getRilRadioTechnology();
-            mNewDataConnectionState = radioTechnologyToDataServiceState(mNewRilRadioTechnology);
-            log("pollStateDone CDMA STATE_IN_SERVICE mNewRilRadioTechnology = " +
-                    mNewRilRadioTechnology + " mNewDataConnectionState = " +
-                    mNewDataConnectionState);
-        }
+        mNewRilRadioTechnology = mLteSS.getRilRadioTechnology();
+        mNewDataConnectionState = mLteSS.getState();
+        newSS.setRadioTechnology(mNewRilRadioTechnology);
+        log("pollStateDone CdmaLTEServiceState STATE_IN_SERVICE newNetworkType = "
+                + mNewDataConnectionState);
 
         // TODO: Add proper support for LTE Only, we should be looking at
         //       the preferred network mode, to know when newSS state should