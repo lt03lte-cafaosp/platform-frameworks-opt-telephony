2ed96edf084328d4cf36ae4f0f9ca0bdc9bdb6b8
Sandeep Gutta <sangutta@codeaurora.org>
2013-05-13 21:55:13 +0530

    Telephony: While handling airplane mode request hangup voice calls first
    While handling airplane mode request in Telephony, follow
    below sequence.
    
    1. Disconnect all voice calls.
    2. Disconnect packet data calls.
    3. Send offline mode request to RIL.
    
    Change-Id: I77a0632af87333cdefb129954b4faf9c3258a036
    CRs-fixed: 465997

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -486,6 +486,12 @@ public class GsmServiceStateTracker extends ServiceStateTracker {
             && cm.getRadioState() == CommandsInterface.RadioState.RADIO_OFF) {
             cm.setRadioPower(true, null);
         } else if (!mDesiredPowerState && cm.getRadioState().isOn()) {
+            // hang up all active voice calls
+            if (phone.isInCall()) {
+                phone.mCT.ringingCall.hangupIfAlive();
+                phone.mCT.backgroundCall.hangupIfAlive();
+                phone.mCT.foregroundCall.hangupIfAlive();
+            }
             // If it's on and available and we want it off gracefully
             DataConnectionTracker dcTracker = phone.mDataConnectionTracker;
             powerOffRadioSafely(dcTracker);
@@ -494,13 +500,6 @@ public class GsmServiceStateTracker extends ServiceStateTracker {
 
     @Override
     protected void hangupAndPowerOff() {
-        // hang up all active voice calls
-        if (phone.isInCall()) {
-            phone.mCT.ringingCall.hangupIfAlive();
-            phone.mCT.backgroundCall.hangupIfAlive();
-            phone.mCT.foregroundCall.hangupIfAlive();
-        }
-
         cm.setRadioPower(false, null);
     }
 
