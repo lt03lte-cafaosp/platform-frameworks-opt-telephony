4923a5b1e344a8b6155e4ebdf7de96e50e7857c7
Garik Badalyan <garikb@codeaurora.org>
2013-05-15 10:45:52 -0700

    Telephony: Remove Upgrade Call dialog when the request times out.
    While in IMS call user can send Upgrade Call request to upgrade voice
    call to video call. The request displays Upgrade Call dialog on the
    other side asking the user to upgrade the call. The dialog stays on
    if the user chooses not to press on YES or NO buttons.
    
    Remove the Upgrade Call dialog after the request times out in QMI-RIL.
    
    Change-Id: I43cd76729357d2ca47cdb1b932e81766095be01a
    CRs-Fixed: 375159

--- src/java/com/android/internal/telephony/CallModify.java
+++ src/java/com/android/internal/telephony/CallModify.java
@@ -28,22 +28,41 @@
 package com.android.internal.telephony;
 
 public class CallModify {
+    // Keep this error codes in sync with error codes defined in
+    // imsIF.proto file.
+    public static int E_SUCCESS = 0;
+    public static int E_CANCELLED = 7;
+    public static int E_UNUSED = 16;
+
     public int call_index;
 
     public CallDetails call_details;
 
+    public int error;
+
     public CallModify() {
-        call_index = 0;
-        call_details = new CallDetails();
+        this(new CallDetails(), 0);
     }
 
     public CallModify(CallDetails callDetails, int callIndex) {
-        call_index = callIndex;
+        this(callDetails, callIndex, E_SUCCESS);
+    }
+
+    public CallModify(CallDetails callDetails, int callIndex, int err) {
         call_details = callDetails;
+        call_index = callIndex;
+        error = err;
     }
 
     public void setCallDetails(CallDetails calldetails) {
         call_details = new CallDetails(calldetails);
+    }
+
+    /**
+     * @return true if the message is sent to notify about the error.
+     */
+    public boolean error() {
+        return this.error != E_UNUSED && this.error != E_SUCCESS;
     }
 
     /**
@@ -52,6 +71,7 @@ public class CallModify {
     @Override
     public String toString() {
         return (" " + call_index
-                + " " + call_details);
+                + " " + call_details
+                + " " + error);
     }
 }
