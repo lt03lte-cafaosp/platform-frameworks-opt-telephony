f243ea55fe3cab57335954e5dc24dc8f4b2bf274
tianhaiyan <tianhaiyan@hisensecom.com>
2013-05-28 14:07:48 +0800

    decrease network register time when booting

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3111,7 +3111,7 @@ public final class RIL extends BaseCommands implements CommandsInterface {
                 if (RILJ_LOGD) unsljLogRet(response, ret);
 
                 // Initial conditions
-                setRadioPower(false, null);
+                setRadioPower(true, null);
                 //setPreferredNetworkType(mPreferredNetworkType, null);
                 //setCdmaSubscriptionSource(mCdmaSubscription, null);
                 notifyRegistrantsRilConnectionChanged(((int[])ret)[0]);
