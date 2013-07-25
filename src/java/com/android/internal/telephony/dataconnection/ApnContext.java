gp-b2g/frameworks_base

Telephony: DataProfile priorities/arbitration
* Add flag to disable MPDN support
* Introduce priorities for different APN Types
* Create data calls in a prioritized order, tearing
 down lower priority calls if required.

Change-Id: I12d4c8bddce27477fcb244e0f873dbecf17ae116

telephony/java/com/android/internal/telephony/ApnContext.java
@@ -32,6 +32,8 @@ public class ApnContext {
 
     private final String mApnType;
 
+    private final int mPriority;
+
     private DataConnectionTracker.State mState;
 
     private ArrayList<DataProfile> mWaitingApns = null;
@@ -59,6 +61,7 @@ public class ApnContext {
 
     public ApnContext(String apnType, String logTag) {
         mApnType = apnType;
+        mPriority = DataConnectionTracker.mApnPriorities.get(apnType);
         mState = DataConnectionTracker.State.IDLE;
         setReason(Phone.REASON_DATA_ENABLED);
         mDataEnabled = new AtomicBoolean(false);
@@ -134,6 +137,22 @@ public class ApnContext {
 
     public synchronized ArrayList<DataProfile> getWaitingApns() {
         return mWaitingApns;
+    }
+
+    public synchronized int getPriority() {
+        return mPriority;
+    }
+
+    public synchronized boolean isHigherPriority(ApnContext context) {
+        return this.mPriority > context.getPriority();
+    }
+
+    public synchronized boolean isLowerPriority(ApnContext context) {
+        return this.mPriority < context.getPriority();
+    }
+
+    public synchronized boolean isEqualPriority(ApnContext context) {
+        return this.mPriority == context.getPriority();
     }
 
     public synchronized void setState(DataConnectionTracker.State s) {

lt03lte-cafaosp/platform-frameworks-opt-telephony

  down lower priority calls if required.
Conflicts:
	src/java/com/android/internal/telephony/Phone.java
	src/java/com/android/internal/telephony/cdma/CdmaDataConnectionTracker.java
	src/java/com/android/internal/telephony/dataconnection/ApnContext.java
	src/java/com/android/internal/telephony/dataconnection/DcTracker.java
	src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
Change-Id: If7db2c8fa00b604c026f50797e8a45ca8a11158b

src/java/com/android/internal/telephony/dataconnection/ApnContext.java
@@ -42,6 +42,8 @@ public class ApnContext {
     private DctConstants.State mState;
 
     private ArrayList<DataProfile> mWaitingDataProfiles = null;
+    private final int mPriority;
+
 
     /** A zero indicates that all waiting APNs had a permanent error */
     private AtomicInteger mWaitingApnsPermanentFailureCountDown;
@@ -67,6 +69,7 @@ public class ApnContext {
     public ApnContext(String DataProfileType, String logTag) {
         mDataProfileType = DataProfileType;
         mState = DctConstants.State.IDLE;
+        mPriority = DcTrackerBase.mApnPriorities.get(mDataProfileType);
         setReason(Phone.REASON_DATA_ENABLED);
         mDataEnabled = new AtomicBoolean(false);
         mDependencyMet = new AtomicBoolean(true);
@@ -141,6 +144,22 @@ public class ApnContext {
 
     public synchronized ArrayList<DataProfile> getWaitingApns() {
         return mWaitingDataProfiles;
+    }
+
+    public synchronized int getPriority() {
+        return mPriority;
+    }
+
+    public synchronized boolean isHigherPriority(ApnContext context) {
+        return this.mPriority > context.getPriority();
+    }
+
+    public synchronized boolean isLowerPriority(ApnContext context) {
+        return this.mPriority < context.getPriority();
+    }
+
+    public synchronized boolean isEqualPriority(ApnContext context) {
+        return this.mPriority == context.getPriority();
     }
 
     public synchronized void setState(DctConstants.State s) {