546b6b39698edb5352da177e9fb4a99da06aae52
Abhinav Gurram <c_abhina@codeaurora.org>
2013-12-09 16:09:58 +0530

    Revert "Revert "Telephony(MSIM): Remain in LCH if call on other sub is ended remotely.""
    This reverts commit 3572cd1acdda10e496918503638ad167e891f537
    
    Change-Id: Ibe52ca520a8d2597a9fd91574b61f3a773790dab

--- src/java/com/android/internal/telephony/CallManager.java
+++ src/java/com/android/internal/telephony/CallManager.java
@@ -2189,4 +2189,8 @@ public class CallManager {
         Rlog.e(LOG_TAG, " getLocalCallHoldStatus for subscription not supported");
         return false;
     }
+
+    public void deactivateLchState(int sub) {
+        Rlog.e(LOG_TAG, " deactivateLchState not supported");
+    }
 }
