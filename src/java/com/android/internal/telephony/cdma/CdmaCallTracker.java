36a3a3c026032e8aaec806e5bee19f3a85e22cd5
mengsun <msun@codeaurora.org>
2014-03-11 17:54:52 +0800

    Fix the duration is not reset when the call is accepted.
    Notify phone state changed when the duration is reset in CDMA.
    CRs-Fixed: 626591
    
    Change-Id: I2c92838f292c5fbe71fa8088f6995099b534d26c

--- src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaCallTracker.java
@@ -110,6 +110,7 @@ public final class CdmaCallTracker extends CallTracker {
             CdmaConnection c = (CdmaConnection) mForegroundCall.getLatestConnection();
             if (c.getDurationMillis() > 0 && !c.isConnectionTimerReset() && !c.isIncoming()) {
                 c.resetConnectionTimer();
+                mPhone.notifyPreciseCallStateChanged();
             }
         }
     }
