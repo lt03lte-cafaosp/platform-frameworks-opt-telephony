LineageOS/android_frameworks_opt_telephony

Set carrierName of subInfo to null while creating/updating subInfo.
Bug: 19476304
Change-Id: Ia16465584cf31858bbf1ae36cfdbf21184de10d7

src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -328,8 +328,6 @@ public class SubscriptionInfoUpdater extends Handler {
 
             SubscriptionInfo subInfo = mSubscriptionManager.getActiveSubscriptionInfo(subId);
             String nameToSet;
-            String CarrierName = tm.getSimOperatorNumericForSubscription(subId);
-            logd("CarrierName = " + CarrierName);
             String simCarrierName = tm.getSimOperatorNameForSubscription(subId);
             ContentValues name = new ContentValues(1);
 
@@ -342,14 +340,10 @@ public class SubscriptionInfoUpdater extends Handler {
                 }
                 name.put(SubscriptionManager.DISPLAY_NAME, nameToSet);
                 logd("sim name = " + nameToSet);
+                contentResolver.update(SubscriptionManager.CONTENT_URI, name,
+                        SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID
+                        + "=" + Long.toString(subId), null);
             }
-            name.put(SubscriptionManager.CARRIER_NAME,
-                    !TextUtils.isEmpty(simCarrierName) ? simCarrierName :
-                    mContext.getString(com.android.internal.R.string.unknownName));
-            contentResolver.update(SubscriptionManager.CONTENT_URI, name,
-                    SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID
-                    + "=" + Long.toString(subId), null);
-            logd("carrier name = " + simCarrierName);
 
             /* Update preferred network type and network selection mode on SIM change.
              * Storing last subId in SharedPreference for now to detect SIM change. */

lt03lte-cafaosp/platform-frameworks-opt-telephony

Conflicts:
	src/java/com/android/internal/telephony/SubscriptionController.java
	src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java

src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -411,8 +411,6 @@ public class SubscriptionInfoUpdater extends Handler {
 
             SubscriptionInfo subInfo = mSubscriptionManager.getActiveSubscriptionInfo(subId);
             String nameToSet;
-            String CarrierName = TelephonyManager.getDefault().getSimOperator(subId);
-            logd("CarrierName = " + CarrierName);
             String simCarrierName = TelephonyManager.getDefault()
                     .getSimOperatorNameForSubscription(subId);
             ContentValues name = new ContentValues(1);
@@ -426,15 +424,10 @@ public class SubscriptionInfoUpdater extends Handler {
                 }
                 name.put(SubscriptionManager.DISPLAY_NAME, nameToSet);
                 logd("sim name = " + nameToSet);
+                contentResolver.update(SubscriptionManager.CONTENT_URI, name,
+                        SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID
+                        + "=" + Integer.toString(subId), null);
             }
-            name.put(SubscriptionManager.CARRIER_NAME,
-                    !TextUtils.isEmpty(simCarrierName) ? simCarrierName :
-                    mContext.getString(com.android.internal.R.string.unknownName));
-            contentResolver.update(SubscriptionManager.CONTENT_URI, name,
-                    SubscriptionManager.UNIQUE_KEY_SUBSCRIPTION_ID
-                    + "=" + Integer.toString(subId), null);
-            logd("carrier name = " + simCarrierName);
-
         } else {
             logd("Invalid subId, could not update ContentResolver");
         }