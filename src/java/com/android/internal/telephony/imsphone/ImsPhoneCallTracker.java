acfd7823faef80c231944113b296cd8a9f957535
Muhammed Siju <msiju@codeaurora.org>
2015-04-22 19:54:50 +0530

    IMS: Notify supplementary service failed for HOLD/RESUME failure.
    For IMS call hold/resume failure, notify the apps by calling
    notifySuppServiceFailed.
    
    Change-Id: Ic547a23098b9d3dc081c2ed99d1bafb6057db1e0
    CRs-Fixed: 825875

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1290,6 +1290,7 @@ public final class ImsPhoneCallTracker extends CallTracker {
                     }
                 }
             }
+            mPhone.notifySuppServiceFailed(Phone.SuppService.SWITCH);
         }
 
         @Override
@@ -1302,7 +1303,7 @@ public final class ImsPhoneCallTracker extends CallTracker {
 
         @Override
         public void onCallResumeFailed(ImsCall imsCall, ImsReasonInfo reasonInfo) {
-            // TODO : What should be done?
+            mPhone.notifySuppServiceFailed(Phone.SuppService.SWITCH);
         }
 
         @Override
