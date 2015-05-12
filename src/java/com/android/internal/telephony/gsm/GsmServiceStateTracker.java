efbc245086c14340e272340170a785defb762ec3
caoyang0505 <caoyang0505@thundersoft.com>
2015-05-12 18:58:05 +0800

    telephony-common: Display International Roaming pop-up Message for given camped network.
    Add copyright.
    
    Change-Id: I8cbf44462c19c16cfb1fc428d28a0433aa1720cb

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Copyright (c) 2015, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
  */
 
 package com.android.internal.telephony.gsm;
