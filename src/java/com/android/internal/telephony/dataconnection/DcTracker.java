7177fa8e83223c8009ac37aeeaddc8b3156201fc
Muhammed Siju <msiju@codeaurora.org>
2015-05-28 17:13:31 +0530

    Pass updated preferred apn with set initial attach request.
    When preferred apn is changed, set initial attach apn is
    performed even before mPreferredApn is updated.
    This causes wrong apn to be used for initial attach in
    case there is no ia type apn available.
    So retrieve the preferred apn directly from db and use it
    while deciding on initial attach apn.
    
    Change-Id: I7f6f56bfcf2bb2f4e7e68237d16910692ed0d530
    CRs-Fixed: 844034

--- src/java/com/android/internal/telephony/dataconnection/DcTracker.java
+++ src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -199,7 +199,7 @@ public final class DcTracker extends DcTrackerBase {
             switch (msg.what) {
                 case EVENT_3GPP_RECORDS_LOADED:
                     log("EVENT_3GPP_RECORDS_LOADED");
-                    onSimRecordsLoaded();
+                    setAttachApn();
                     break;
             }
         }
@@ -1468,7 +1468,7 @@ public final class DcTracker extends DcTrackerBase {
      */
     private void onApnChanged() {
         if (DBG) log("onApnChanged: tryRestartDataConnections");
-        setInitialAttachApn(create3gppApnsList(), mSimRecords.get());
+        setAttachApn();
         tryRestartDataConnections(true, Phone.REASON_APN_CHANGED);
     }
 
@@ -2534,7 +2534,7 @@ public final class DcTracker extends DcTrackerBase {
             // TODO: What is the right behavior?
             //notifyNoData(DataConnection.FailCause.MISSING_UNKNOWN_APN);
         } else {
-            mPreferredApn = getPreferredApn();
+            mPreferredApn = getPreferredApn(mAllApnSettings);
             if (mPreferredApn != null && !mPreferredApn.numeric.equals(operator)) {
                 mPreferredApn = null;
                 setPreferredApn(-1);
@@ -2870,9 +2870,9 @@ public final class DcTracker extends DcTrackerBase {
         }
     }
 
-    private ApnSetting getPreferredApn() {
-        if (mAllApnSettings.isEmpty()) {
-            log("getPreferredApn: X not found mAllApnSettings.isEmpty");
+    private ApnSetting getPreferredApn(ArrayList<ApnSetting> apnList) {
+        if (apnList == null || apnList.isEmpty()) {
+            log("getPreferredApn: X not found apnList.isEmpty");
             return null;
         }
 
@@ -2894,7 +2894,7 @@ public final class DcTracker extends DcTrackerBase {
             int pos;
             cursor.moveToFirst();
             pos = cursor.getInt(cursor.getColumnIndexOrThrow(Telephony.Carriers._ID));
-            for(ApnSetting p : mAllApnSettings) {
+            for(ApnSetting p : apnList) {
                 log("getPreferredApn: apnSetting=" + p);
                 if (p.id == pos && p.canHandleType(mRequestedApnType)) {
                     log("getPreferredApn: X found apnSetting" + p);
@@ -3165,8 +3165,10 @@ public final class DcTracker extends DcTrackerBase {
         }
     }
 
-    private void onSimRecordsLoaded() {
-        setInitialAttachApn(create3gppApnsList(), mSimRecords.get());
+    private void setAttachApn() {
+        ArrayList<ApnSetting> apnList = create3gppApnsList();
+        ApnSetting preferApn = getPreferredApn(apnList);
+        setInitialAttachApn(apnList, preferApn, mSimRecords.get());
     }
 
     public void update() {
