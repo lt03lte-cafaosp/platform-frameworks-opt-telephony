9276f9c84731fc4e3d8094fe2946d663ac4e9359
Suresh Koleti <skolet@codeaurora.org>
2015-06-03 16:26:01 +0530

    Ims: Show HD icon based on Volte/VT capabilities
    As per current design HD icon is shown in status bar depends on
    IMS registration state which is not correct as IMS registration
    will be in registered state (for SMS over IMS purpose) even after SRVCC.
    Ex: SRVCC from LTE to WCDMA, modem will not report as IMS deregistered.
    
    Show HD icon based on Volte/VT capabilities.
    
    Conflicts:
    	src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
    
    Change-Id: If05d56e2fb070a46d232175321be1a697a32b848
    CRs-Fixed: 843107

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -1473,33 +1473,24 @@ public final class ImsPhoneCallTracker extends CallTracker {
         public void onImsConnected() {
             if (DBG) log("onImsConnected");
             mPhone.setServiceState(ServiceState.STATE_IN_SERVICE);
-            mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
-                VoLteServiceState.IMS_REGISTERED));
         }
 
         @Override
         public void onImsDisconnected() {
             if (DBG) log("onImsDisconnected");
             mPhone.setServiceState(ServiceState.STATE_OUT_OF_SERVICE);
-            mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
-                VoLteServiceState.IMS_UNREGISTERED));
         }
 
         @Override
         public void onImsResumed() {
             if (DBG) log("onImsResumed");
             mPhone.setServiceState(ServiceState.STATE_IN_SERVICE);
-            mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
-                VoLteServiceState.IMS_REGISTERED));
-
         }
 
         @Override
         public void onImsSuspended() {
             if (DBG) log("onImsSuspended");
             mPhone.setServiceState(ServiceState.STATE_OUT_OF_SERVICE);
-            mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
-                VoLteServiceState.IMS_UNREGISTERED));
         }
 
         @Override
@@ -1507,6 +1498,7 @@ public final class ImsPhoneCallTracker extends CallTracker {
                 int[] enabledFeatures, int[] disabledFeatures) {
             if (serviceClass == ImsServiceClass.MMTEL) {
                 boolean tmpIsVtEnabled = mIsVtEnabled;
+                boolean tmpIsVolteEnabled = mIsVolteEnabled;
 
                 if (disabledFeatures[ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_LTE] ==
                         ImsConfig.FeatureConstants.FEATURE_TYPE_VOICE_OVER_LTE ||
@@ -1547,6 +1539,16 @@ public final class ImsPhoneCallTracker extends CallTracker {
 
                 if (tmpIsVtEnabled != mIsVtEnabled) {
                     mPhone.notifyForVideoCapabilityChanged(mIsVtEnabled);
+                }
+
+                if (tmpIsVolteEnabled != mIsVolteEnabled || tmpIsVtEnabled != mIsVtEnabled) {
+                    if (mIsVolteEnabled || mIsVtEnabled) {
+                        mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
+                                VoLteServiceState.IMS_REGISTERED));
+                    } else {
+                        mPhone.notifyVoLteServiceStateChanged(new VoLteServiceState(
+                                VoLteServiceState.IMS_UNREGISTERED));
+                    }
                 }
             }
 
