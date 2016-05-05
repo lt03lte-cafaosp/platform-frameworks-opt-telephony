d865bcf5a2241a1a3e4089924b9c2e5a6c5971f8
Hari Veerubhotla <vsubrahm@codeaurora.org>
2016-05-05 12:48:24 +0530

    Display APN for roaming carrier
    Show only APNs belonging to carrier (mno/mvno) under apn settings.
    Add apns for carrier and display in Settings
    
    Change-Id: Ia813551e19591e7113ed28cf86f3bef6b784b568
    CRs-Fixed: 993196

--- src/java/com/android/internal/telephony/dataconnection/ApnSetting.java
+++ src/java/com/android/internal/telephony/dataconnection/ApnSetting.java
@@ -19,10 +19,11 @@
 package com.android.internal.telephony.dataconnection;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.RILConstants;
-
+import com.android.internal.telephony.uicc.IccRecords;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -34,6 +35,7 @@ public class ApnSetting {
 
     static final String V2_FORMAT_REGEX = "^\\[ApnSettingV2\\]\\s*";
     static final String V3_FORMAT_REGEX = "^\\[ApnSettingV3\\]\\s*";
+    static final String TAG = "ApnSetting";
 
     public final String carrier;
     public final String apn;
@@ -331,6 +333,63 @@ public class ApnSetting {
                     (t.equalsIgnoreCase(PhoneConstants.APN_TYPE_DEFAULT) &&
                     type.equalsIgnoreCase(PhoneConstants.APN_TYPE_HIPRI))) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean imsiMatches(String imsiDB, String imsiSIM) {
+        // Note: imsiDB value has digit number or 'x' character for seperating USIM information
+        // for MVNO operator. And then digit number is matched at same order and 'x' character
+        // could replace by any digit number.
+        // ex) if imsiDB inserted '310260x10xxxxxx' for GG Operator,
+        //     that means first 6 digits, 8th and 9th digit
+        //     should be set in USIM for GG Operator.
+        int len = imsiDB.length();
+        int idxCompare = 0;
+
+        if (len <= 0) return false;
+        if (len > imsiSIM.length()) return false;
+
+        for (int idx=0; idx<len; idx++) {
+            char c = imsiDB.charAt(idx);
+            if ((c == 'x') || (c == 'X') || (c == imsiSIM.charAt(idx))) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean mvnoMatches(IccRecords r, String mvnoType, String mvnoMatchData) {
+        if (mvnoType.equalsIgnoreCase("spn")) {
+            if ((r.getServiceProviderName() != null) &&
+                    r.getServiceProviderName().equalsIgnoreCase(mvnoMatchData)) {
+                return true;
+            }
+        } else if (mvnoType.equalsIgnoreCase("imsi")) {
+            String imsiSIM = r.getIMSI();
+            if ((imsiSIM != null) && imsiMatches(mvnoMatchData, imsiSIM)) {
+                return true;
+            }
+        } else if (mvnoType.equalsIgnoreCase("gid")) {
+            String gid1 = r.getGid1();
+            int mvno_match_data_length = mvnoMatchData.length();
+            if ((gid1 != null) && (gid1.length() >= mvno_match_data_length) &&
+                    gid1.substring(0, mvno_match_data_length).equalsIgnoreCase(mvnoMatchData)) {
+                return true;
+            }
+        } else if (mvnoType.equalsIgnoreCase("iccid")) {
+            String iccId = r.getIccId();
+            if (iccId != null) {
+                String[] mvnoIccidList = mvnoMatchData.split(",");
+                for (String mvnoIccid : mvnoIccidList) {
+                    if (iccId.startsWith(mvnoIccid.trim())) {
+                        Log.d(TAG, "mvno icc id match found");
+                        return true;
+                    }
+                }
             }
         }
         return false;
